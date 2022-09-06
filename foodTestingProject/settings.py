c408c9be20be985d884a88c7d3cc0b7729f202ff
Hyeokmin Kwon <khmkoh@naver.com>
2022-09-07 01:52:23 +0900

    modify setting

--- foodTestingProject/settings.py
+++ foodTestingProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-lt9n3-euj&j2xkgdf_cc2k#v^%2%sk3@fxeag%t1rzq(eqrjt+"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
