7d69dac02c754f488feaa302c54fa4ce44e67f9f
Hyeokmin Kwon <khmkoh@naver.com>
2022-09-25 02:19:19 +0900

    modified table and add img

--- app/admin.py
+++ app/admin.py
@@ -4,9 +4,13 @@ from django.contrib import admin
 from .models import Customer
 from .models import Market
 from .models import Post
+from .models import Quesbymarket
+from .models import Questionlist
 from .models import Review
 
 admin.site.register(Customer)
 admin.site.register(Market)
 admin.site.register(Post)
+admin.site.register(Quesbymarket)
+admin.site.register(Questionlist)
 admin.site.register(Review)
--- app/models.py
+++ app/models.py
@@ -8,9 +8,7 @@ class Customer(models.Model):
     password = models.CharField(max_length=15)
     email = models.CharField(max_length=64)
     name = models.CharField(max_length=17, blank=True, null=True)
-    nickname = models.CharField(max_length=45, blank=True, null=True)
     gender = models.CharField(max_length=1)
-    profile_photo = models.TextField(blank=True, null=True)
     born_date = models.CharField(max_length=10, blank=True, null=True)
     customer_type = models.CharField(max_length=1)
 
@@ -20,17 +18,19 @@ class Customer(models.Model):
 
 
 class Market(models.Model):
-    reg_num = models.IntegerField(primary_key=True)
+    reg_num = models.CharField(primary_key=True, max_length=12)
     name = models.CharField(max_length=20)
-    menu_photo = models.TextField()
+    market_photo = models.CharField(max_length=255)
     start_date = models.BigIntegerField()
+    period = models.BigIntegerField()
     customer_uuid = models.ForeignKey(Customer, models.DO_NOTHING, db_column='customer_uuid')
     location = models.CharField(max_length=200)
     latitude = models.FloatField()
     longtitude = models.FloatField()
     phone_num = models.CharField(max_length=13)
     category = models.CharField(max_length=10)
-    hashtag = models.CharField(max_length=1000)
+    business_hours = models.BigIntegerField()
+    congestion_degree = models.CharField(max_length=45, blank=True, null=True)
 
     class Meta:
         managed = False
@@ -43,27 +43,48 @@ class Post(models.Model):
     write_market = models.ForeignKey(Market, models.DO_NOTHING, db_column='write_market')
     writer_uuid = models.ForeignKey(Customer, models.DO_NOTHING, db_column='writer_uuid')
     hope_price = models.IntegerField()
-    bargain_price = models.IntegerField()
+    discounted_price = models.IntegerField()
     start_date = models.BigIntegerField()
     end_date = models.BigIntegerField()
     ingredients = models.CharField(max_length=1000)
     post_date = models.BigIntegerField()
     update_date = models.BigIntegerField()
     contents = models.CharField(max_length=1000)
+    menu_photo = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'post'
 
 
+class Quesbymarket(models.Model):
+    market_reg_num = models.OneToOneField(Market, models.DO_NOTHING, db_column='market_reg_num', primary_key=True)
+    ques_uuid = models.ForeignKey('Questionlist', models.DO_NOTHING, db_column='ques_uuid')
+
+    class Meta:
+        managed = False
+        db_table = 'quesbymarket'
+        unique_together = (('market_reg_num', 'ques_uuid'),)
+
+
+class Questionlist(models.Model):
+    ques_uuid = models.CharField(primary_key=True, max_length=36)
+    market_reg_num = models.ForeignKey(Market, models.DO_NOTHING, db_column='market_reg_num')
+    contents = models.CharField(max_length=200)
+    fast_response = models.CharField(max_length=200, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'questionlist'
+
+
 class Review(models.Model):
     review_uuid = models.CharField(primary_key=True, max_length=36)
     writer_uuid = models.ForeignKey(Customer, models.DO_NOTHING, db_column='writer_uuid')
     market_reg_num = models.ForeignKey(Market, models.DO_NOTHING, db_column='market_reg_num')
-    contents = models.CharField(max_length=1000)
+    ques_uuid = models.ForeignKey(Questionlist, models.DO_NOTHING, db_column='ques_uuid', blank=True, null=True)
+    review_line = models.CharField(max_length=500)
     review_date = models.BigIntegerField()
-    star = models.IntegerField()
-    hashtag = models.CharField(max_length=1000)
 
     class Meta:
         managed = False
